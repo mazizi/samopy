23792c9de888a5302f920c09554fd117644b8e9b
Nathan Gotz <nlgotz+github@gotzweb.com>
2016-09-15 08:11:40 -0500

    Added Ability to run CLI commands

--- Samopy/samopy.py
+++ Samopy/samopy.py
@@ -2,6 +2,7 @@ import requests
 from jinja2 import Environment, FileSystemLoader
 import os
 
+
 class SAMOpy(object):
     """ Documentation HERE
 
@@ -29,42 +30,49 @@ class SAMOpy(object):
 
     def create(self):
         ENV = Environment(loader=FileSystemLoader(
-              os.path.join(os.path.dirname(__file__), "templates")))
+                          os.path.join(os.path.dirname(__file__), "templates")))
         template = ENV.get_template("create.j2")
-        data = template.render(management=self.management,config=var)
+        data = template.render(management=self.management, config=var)
         return self._req(data)
 
     def read(self, var):
         ENV = Environment(loader=FileSystemLoader(
-              os.path.join(os.path.dirname(__file__), "templates")))
+                          os.path.join(os.path.dirname(__file__), "templates")))
         template = ENV.get_template("read.j2")
-        data = template.render(management=self.management,config=var)
+        data = template.render(management=self.management, config=var)
         return self._req(data)
 
     def update(self):
         ENV = Environment(loader=FileSystemLoader(
-              os.path.join(os.path.dirname(__file__), "templates")))
+                          os.path.join(os.path.dirname(__file__), "templates")))
         template = ENV.get_template("update.j2")
-        data = template.render(management=self.management,config=var)
+        data = template.render(management=self.management, config=var)
         return self._req(data)
 
     def delete(self):
         ENV = Environment(loader=FileSystemLoader(
-              os.path.join(os.path.dirname(__file__), "templates")))
+                          os.path.join(os.path.dirname(__file__), "templates")))
         template = ENV.get_template("delete.j2")
-        data = template.render(management=self.management,config=var)
+        data = template.render(management=self.management, config=var)
+        return self._req(data)
+
+    def cli(self, ip_addr, command):
+        ENV = Environment(loader=FileSystemLoader(
+                          os.path.join(os.path.dirname(__file__), "templates")))
+        template = ENV.get_template("cli.j2")
+        data = template.render(ip_addr=ip_addr, command=command)
         return self._req(data)
 
     def filter(self, items):
         ENV = Environment(loader=FileSystemLoader(
-              os.path.join(os.path.dirname(__file__), "templates")))
+                          os.path.join(os.path.dirname(__file__), "templates")))
         template = ENV.get_template("filter.j2")
         data = template.render(items)
         return data
 
     def result_filter(self, items):
         ENV = Environment(loader=FileSystemLoader(
-              os.path.join(os.path.dirname(__file__), "templates")))
+                          os.path.join(os.path.dirname(__file__), "templates")))
         template = ENV.get_template("result_filter.j2")
         data = template.render(items)
         return data
@@ -74,19 +82,30 @@ class SAMOpy(object):
     """
     def get_network_interfaces(self, filter=False, result_filter=False):
         class_name = 'rtr.NetworkInterface'
-        if filter != False:
+        if filter:
             filter_xml = self.filter(filter)
         else:
             filter_xml = None
 
-        if result_filter != False:
+        if result_filter:
             result_attr = result_filter
         else:
-            result_attr = {'result_attr': ['displayedName', 'nodeName', 'objectFullName',
-                                'olcState', 'primaryIPv4Address',
-                                'primaryIPv4PrefixLength'], 'children': True}
+            result_attr = {
+                'result_attr': [
+                    'displayedName',
+                    'nodeName',
+                    'objectFullName',
+                    'olcState',
+                    'primaryIPv4Address',
+                    'primaryIPv4PrefixLength'
+                ],
+                'children': True
+            }
         result_xml = self.result_filter(result_attr)
 
         var = {'class_name': class_name, 'result_filter': result_xml,
                'filter': filter_xml, 'children': True}
         return self.read(var)
+
+    def cli_ping(self, src_addr, dst_addr):
+        return cli(src_addr, "ping {}".format(dst_addr))
